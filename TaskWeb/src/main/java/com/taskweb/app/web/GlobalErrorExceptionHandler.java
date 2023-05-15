9755991b963d6b12cbbbda083a25c3dd811efb9f
alber <alber@192.168.1.176>
2023-05-15 22:45:56 +0200

    haciendo pruebas

--- TaskWeb/src/main/java/com/taskweb/app/web/GlobalErrorExceptionHandler.java
+++ TaskWeb/src/main/java/com/taskweb/app/web/GlobalErrorExceptionHandler.java
@@ -2,22 +2,21 @@ package com.taskweb.app.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class GlobalErrorExceptionHandler {
+public class GlobalErrorExceptionHandler implements ErrorController {
 	
 	private Logger logger = LoggerFactory.getLogger(getClass());
 	
 	@ExceptionHandler(Exception.class)
-    public ResponseEntity<String> exceptionHandler(){
+    public ResponseEntity<String> exceptionHandler(Exception e){
 		logger.debug("REST request error"); 
-        return new ResponseEntity<>("Error!", HttpStatus.BAD_REQUEST);
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error api");
     }
-	
-	
 
 }
